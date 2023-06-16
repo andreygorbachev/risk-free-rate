779c773a47068124fc912532d6fe8fdbe5bdbc1e
Andrey Gorbachev <andreygorbachev@hotmail.com>
2023-06-16 15:00:20 +0100

    Add a test for Swiss Current Index ON.

--- test/saron.cpp
+++ test/saron.cpp
@@ -121,6 +121,74 @@ namespace risk_free_rate
 		}
 	}
 
-	// add tests for Swiss Current Rate ON
+	TEST(saron, SwissCurrentRateON) // better name?
+	{
+		auto ts = parse_csv(
+			SARON,
+			"Date"s,
+			"Swiss Current Rate ON"s,
+			';'
+		);
+
+		// from https://www.six-group.com/en/products-services/the-swiss-stock-exchange/market-data/news-tools/trading-currency-holiday-calendar.html#/
+		const auto BerchtoldDay = calendar::offset_holiday<calendar::named_holiday>{ calendar::NewYearsDay, std::chrono::days{ 1 } };
+		const auto LaborDay = calendar::named_holiday{ std::chrono::May / std::chrono::day{ 1u } }; // should it be in calendar?
+		const auto NationalDay = calendar::named_holiday{ std::chrono::August / std::chrono::day{ 1u } };
+		auto rules = std::unordered_set<const calendar::annual_holiday*>{};
+		rules.insert(&calendar::NewYearsDay);
+		rules.insert(&BerchtoldDay);
+		rules.insert(&calendar::GoodFriday);
+		rules.insert(&calendar::EasterMonday);
+		rules.insert(&LaborDay);
+		rules.insert(&calendar::AscensionDay);
+		rules.insert(&calendar::Whitmonday);
+		rules.insert(&NationalDay);
+//		rules.insert(&calendar::ChristmasEve);
+		rules.insert(&calendar::ChristmasDay);
+		rules.insert(&calendar::BoxingDay); // should it be called it St. Stephen's Day?
+//		rules.insert(&calendar::NewYearsEve);
+		auto hs = calendar::make_holiday_schedule(
+			{ std::chrono::year{ 1999 }, std::chrono::year{ 2024 } },
+			rules
+		);
+		// factor this out?
+
+		const auto r = resets{ move(ts), &Actual360 };
+		const auto from = 1999y / June / 30d;
+		const auto publication = calendar::calendar{
+			calendar::SaturdaySundayWeekend,
+			std::move(hs)
+		};
+		const auto decimal_places = 6u;
+		const auto starting_value = 10'000.0;
+		const auto ci = make_compounded_index2(
+			r,
+			from,
+			publication,
+			decimal_places,
+			starting_value
+		);
+
+		const auto expected = parse_csv(
+			SARONCompoundedIndex,
+			"Date"s,
+			"Swiss Current Index ON"s,
+			';'
+		);
+//		EXPECT_EQ(expected, ci.get_time_series());
+		for (auto d = expected.get_period().get_from();
+			d <= expected.get_period().get_until();
+			d = sys_days{ d } + days{ 1 }
+		)
+		{
+			const auto& o = ci.get_time_series()[d];
+
+			const auto& e = expected[d];
+			if (e)
+				EXPECT_DOUBLE_EQ(*e, *o);
+			else
+				EXPECT_FALSE(o);
+		}
+	}
 
 }
