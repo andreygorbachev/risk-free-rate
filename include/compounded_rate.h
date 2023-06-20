5fb8c5e24a69a4aba94f8f9866b245e723e291bf
Andrey Gorbachev <andreygorbachev@hotmail.com>
2023-06-20 11:38:47 +0100

    Make sure we correct for bad days after the roll.

--- include/compounded_rate.h
+++ include/compounded_rate.h
@@ -47,10 +47,20 @@ namespace risk_free_rate
 		const calendar::calendar& publication
 	) -> std::chrono::year_month_day
 	{
-		return calendar::ModifiedPreceding.adjust(
+		auto result = calendar::ModifiedPreceding.adjust(
 			maturity - T{ term },
 			publication
 		); // please note that 1w version needs Preceding
+
+		// should it be factored out as a function?
+		// could we have !ok for some other reason than expected below?
+		if (!result.ok()) // we have a non-existing day of month (29, 30 or 31)
+			result = std::chrono::year_month_day_last{
+				result.year(),
+				result.month() / std::chrono::last
+			};
+
+		return result;
 	}
 	// or should we do from/until instead of effective/maturity?
 	// are both there functions just an example of a relative date? (which might be better captured as a class)
